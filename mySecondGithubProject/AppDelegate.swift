cbd6ae936616dcac15f64898e6f6a85ef51bd680
Pierre <pierre@gepetto.local>
2018-10-21 21:14:16 +0200

    Initial Commit

---
+++ mySecondGithubProject/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mySecondGithubProject
+//
+//  Created by Pierre on 21/10/2018.
+//  Copyright © 2018 Pierre. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mySecondGithubProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mySecondGithubProject
+//
+//  Created by Pierre on 21/10/2018.
+//  Copyright © 2018 Pierre. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
